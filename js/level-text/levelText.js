8ba050c7803ac950c8ea4e086b0c9d354b7a0f49
Fellryn <kurt.single@gmail.com>
2025-07-01 02:30:36 +1000

    Small dialog changes and CSP.

--- js/level-text/levelText.js
+++ js/level-text/levelText.js
@@ -14,17 +14,17 @@ export const levelText = [
                 /* 2 */ new Line({ text: "We're all about painting here!" }),
                 /* 3 */ new Line({ text: "So, you're probably wondering what the hell is going on, right?" }),
                 /* 4 */ new Line({ text: "Well, you see, this game is all about filling that canvas up with as much paint as possible, and sometimes in a certain pattern!" }),
-                /* 5 */ new Line({ text: "So, are you ready to paint?", tooltipText: [`"Sure am!"`, `"Maybe not..." (Skip Tutorial)`], charAnimAfter: "question" }),
+                /* 5 */ new Line({ text: "So, are you ready to paint?", tooltipText: [`"Sure am!" (Do Tutorial)`, `"Maybe not..." (Skip Tutorial)`], charAnimAfter: "question" }),
                 /* 6 */ new Line({ text: "Well, lets get going then partner!" }),
                 /* 7 */ new Line({ text: "Pick up one of those paint tools by clicking on it!", delayAfterWhole: 3, waitForInput: true, inputEvent: "pickUpTool" }),
                 /* 8 */ new Line({ text: "Good! Now, dip the tool in the bucket so you have some paint on your brush!", delayAfterWhole: 3, waitForInput: true, inputEvent: "putPaintOnTool" }),
                 /* 9 */ new Line({ text: "Now, put it on the canvas and press it down to paint!", delayAfterWhole: 3, waitForInput: true, inputEvent: "putPaintOnCanvas" }),
                 /* 10 */ new Line({ text: "Excellent! Now you're all set!" }),
-                /* 11 */ new Line({ text: "If you just paint this whole wall red you'll be all done, let me know when you're finished!", waitForInput: true, inputEvent: "wallPaintedRed", tooltipText: [`"All done chief!`, "How do I paint again?"] }),
-                /* 12 */ new Line({ text: "That's a shame! Well, goodluck and have a nice day! I was after red paint on the entire wall if you're interested...", waitForInput: true, inputEvent: "wallPaintedRed", tooltipText: [`"Fine, here's your red wall!`, `"Na mate, I'm out!"`], charAnim: "shame", charAnimAfter: "question" }),
+                /* 11 */ new Line({ text: "If you just paint this whole wall red you'll be all done, let me know when you're finished!", waitForInput: true, inputEvent: "wallPaintedRed", tooltipText: [`"All done chief!"`, `"How do I paint again?"`] }),
+                /* 12 */ new Line({ text: "Well now, if you still feel like it, I was after a wall covered completely in that red paint...", waitForInput: true, inputEvent: "wallPaintedRed", tooltipText: [`"Fine, here's your red wall!"`, `"Na mate, I'm out!" (Skip tutorial level)`], charAnim: "shame", charAnimAfter: "question" }),
                 /* 13 */ new Line({ text: "Yep, that's it, nicely done! Now, onto something a bit harder if you're interested! (Press fast forward to go to the next level)" }),
                 /* 14 */ new Line({ text: "Woah now, you need more paint than that! Make sure most of the wall is a solid red color, you may need two coats.", charAnimAfter: "exclaim" }),
-                /* 15 */ new Line({ text: "Well, feel free to paint the wall anyway if you want! (Press fast forward to go to the next level)" })
+                /* 15 */ new Line({ text: "Well, I'll see you tomorrow I suppose! (Press fast forward to go to the next level)" })
                         ],
                     questionAtLine: [[5, 6, 12, 5], [11, 13, 7, 14], [12, 13, 15, 14]],
                     linesNext: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, -1, 11, -1]
